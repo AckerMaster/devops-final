49ca6b33721307ab2244880a62496f445e1bd037
liad <liad@gmail.com>
2025-03-17 11:22:30 +0200

    added more outputs

--- terraform-files/outputs.tf
+++ terraform-files/outputs.tf
@@ -5,3 +5,13 @@ output "instance_ip" {
 output "instance_id" {
   value = aws_instance.builder.id
 }
+
+output "ssh_key_location" {
+  description = "Path to the SSH private key"
+  value       = "${path.module}/liad_ssh_key.pem"
+}
+
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = aws_security_group.liad_security_group_id.id
+}
